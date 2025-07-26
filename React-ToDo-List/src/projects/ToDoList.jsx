a5def7e56f930e98ee29d6dc762216875d5f606c
chetan810 <chetanchoudhary8109@gmail.com>
2025-07-26 12:55:07 +0530

    new component added

--- React-ToDo-List/src/projects/ToDoList.jsx
+++ React-ToDo-List/src/projects/ToDoList.jsx
@@ -1,12 +1,13 @@
 import { IoMdCheckmark } from "react-icons/io";
 import { MdDeleteForever } from "react-icons/md";
-
+import {Git} from '../components/Git'
 
 export const ToDoList =  ({ data , onHandleDeleteToDo}) =>{
     return(
         <li  className="todo-item">
                 <span>{data}</span>
                 <button className="check-btn">
+                  <Git />
                   <IoMdCheckmark />   
                 </button>
                 <button className="delete-btn" onClick = {() =>onHandleDeleteToDo(data)}>
